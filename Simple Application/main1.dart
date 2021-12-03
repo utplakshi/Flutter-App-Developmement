0b3dc7534851f89c7a6767dd4a4216bfe66a7183
utplakshi <71188736+utplakshi@users.noreply.github.com>
2021-12-03 22:10:45 +0530

    03-12-2021

--- Simple Application/main1.dart
+++ Simple Application/main1.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    int days=30;
+    int days = 30;
+    String name = "Utplakshi";
+    /** double pi = 3.14;
+    bool isMale = false;
+    num temp = 30.5; //It can take both integer and double
+    var day = "Tuesday"; //It can take all values
+    const PI = 3.14; //Value of which can't change i.e will remain same
+     */
     return MaterialApp(
-      home:  Material(
+      home: Material(
         child: Center(
           child: Container(
-          child: Text("Welcome to $days days flutter"),
-            ),
+            child: Text("Welcome to $days days flutter $name"),
+          ),
         ),
       ),
     );
